a59fe7c99f88c43b38276d801a79ceabb11b66e5
Piorunes <piorunes@gmail.com>
2016-03-08 10:50:13 +0100

    mavenowe rzeczy (w starych folderach są dawne zadania)

---
+++ mvn/src/main/java/DoubleCalculator.java
@@ -0,0 +1,37 @@
+public class DoubleCalculator {
+
+    public double add(double a, double b )
+    {
+        return a + b;
+    }
+
+    public double sub(double a, double b )
+    {
+        return a - b;
+    }
+
+    public double multi(double a, double b)
+    {
+        return a * b;
+    }
+
+    public double div(double a, double b)
+    {
+        return a/b;
+    }
+
+    boolean greater(double a, double b)
+    {
+        if (a>b) return true;
+        else return false;
+    }
+
+
+
+    public DoubleCalculator()
+    {
+        System.out.println( " Konstruktor Calc: " + this);
+    }
+
+
+}
